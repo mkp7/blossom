8382641c55f9fb612a214f35ad86f9768002af0e
Mehulkumar <m3hul.g4m3r@gmail.com>
2019-05-17 15:36:54 +0530

    Fix check function

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const murmur32 = util.promisify(mmh3.murmur32)
 const bitArr = Buffer.alloc(100000)
 const hashCount = 23
 
-function hash (key, seed) {
-  mmh3.murmur32(key, seed, function (err, hashValue) {
-    if (err) throw err
-  })
-  // return
-}
+// function hash (key, seed) {
+//   mmh3.murmur32(key, seed, function (err, hashValue) {
+//     if (err) throw err
+//   })
+//   // return
+// }
 
 function add (bitArr, key, hashCount, cb) {
   for (let i = 0; i < hashCount; i++) {
@@ -22,7 +22,6 @@ function add (bitArr, key, hashCount, cb) {
         const bitVal = (1 << (7 - (bitIndex % 8)))
         bitArr.writeInt8(byte | bitVal, byteIndex)
         if (i === hashCount - 1) {
-          console.log('added')
           cb()
         }
       })
@@ -30,7 +29,8 @@ function add (bitArr, key, hashCount, cb) {
   }
 }
 
-function check (bitArr, key, hashCount) {
+function check (bitArr, key, hashCount, cb) {
+  let present = 0
   for (let i = 0; i < hashCount; i++) {
     murmur32(key, i)
       .then(hashValue => {
@@ -38,14 +38,22 @@ function check (bitArr, key, hashCount) {
         const byteIndex = parseInt(bitIndex / 8)
         const byte = bitArr[byteIndex]
         const bitVal = (1 << (7 - (bitIndex % 8)))
-        if ((byte & bitVal) === 0) {
-          console.log(`${key} is not present`)
-        } else {
-          console.log(`${key} is present`)
+        if ((byte & bitVal) !== 0) {
+          present += 1
+        }
+        if (i === hashCount - 1) {
+          if (present === hashCount) {
+            cb(true)
+          } else {
+            cb(false)
+          }
         }
       })
       .catch(err => { throw err })
   }
 }
 
-add(bitArr, 'mac', hashCount, () => check(bitArr, 'ma', hashCount))
+add(bitArr, 'mac', hashCount, () => {
+  console.log('added')
+  check(bitArr, 'mc', hashCount, console.log)
+})

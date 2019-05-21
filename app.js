713c846cbb48f6abf347db1e4ecca3394b6b49a6
Mehulkumar <m3hul.g4m3r@gmail.com>
2019-05-21 08:08:31 +0530

    Refactor, fixed a bug with calc byte position

--- app.js
+++ app.js
@@ -2,25 +2,18 @@ const util = require('util')
 const mmh3 = require('murmurhash3')
 const murmur32 = util.promisify(mmh3.murmur32)
 
-const bitArr = Buffer.alloc(100000)
-const hashCount = 23
+const byteArr = Buffer.alloc(10)
+const hashCount = 2
 
-// function hash (key, seed) {
-//   mmh3.murmur32(key, seed, function (err, hashValue) {
-//     if (err) throw err
-//   })
-//   // return
-// }
-
-function add (bitArr, key, hashCount, cb) {
+function add (byteArr, key, hashCount, cb) {
   for (let i = 0; i < hashCount; i++) {
     murmur32(key, i)
       .then(hashValue => {
-        const bitIndex = (hashValue % 100000 * 8) - 1
+        const bitIndex = (hashValue % 10 * 8)
         const byteIndex = parseInt(bitIndex / 8)
-        const byte = bitArr[byteIndex]
+        const byte = byteArr[byteIndex]
         const bitVal = (1 << (7 - (bitIndex % 8)))
-        bitArr.writeInt8(byte | bitVal, byteIndex)
+        byteArr.writeUInt8(byte | bitVal, byteIndex)
         if (i === hashCount - 1) {
           cb()
         }
@@ -29,31 +22,30 @@ function add (bitArr, key, hashCount, cb) {
   }
 }
 
-function check (bitArr, key, hashCount, cb) {
+function check (byteArr, key, hashCount, cb) {
   let present = 0
   for (let i = 0; i < hashCount; i++) {
     murmur32(key, i)
       .then(hashValue => {
-        const bitIndex = (hashValue % 100000 * 8) - 1
+        const bitIndex = (hashValue % 10 * 8)
         const byteIndex = parseInt(bitIndex / 8)
-        const byte = bitArr[byteIndex]
+        const byte = byteArr[byteIndex]
         const bitVal = (1 << (7 - (bitIndex % 8)))
         if ((byte & bitVal) !== 0) {
           present += 1
         }
         if (i === hashCount - 1) {
-          if (present === hashCount) {
-            cb(true)
-          } else {
-            cb(false)
-          }
+          cb(present === hashCount)
         }
       })
       .catch(err => { throw err })
   }
 }
 
-add(bitArr, 'mac', hashCount, () => {
-  console.log('added')
-  check(bitArr, 'mc', hashCount, console.log)
+add(byteArr, 'mac', hashCount, () => {
+  console.log('mac is added')
+  check(byteArr, 'mc', hashCount, isThere => (console.log(`mc is present: ${isThere}`)))
+  check(byteArr, 'mac', hashCount, isThere => (console.log(`mac is present: ${isThere}`)))
+  check(byteArr, 'mca', hashCount, isThere => (console.log(`mca is present: ${isThere}`)))
+  check(byteArr, 'mec', hashCount, isThere => (console.log(`mec is present: ${isThere}`)))
 })
